6af49071a61d26b19d056c320d4cdf4ea3cfa755
Humberto Brites <humberto00b@gmail.com>
2022-12-15 20:26:21 -0300

    atualizações

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ViewChild } from '@angular/core';
+import { AgGridAngular } from 'ag-grid-angular';
+import { CellClickedEvent, ColDef, GridReadyEvent } from 'ag-grid-community';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ag_grid';
+
+  // Cada definição de coluna resulta em uma coluna.
+  public columnDefs: ColDef[] = [
+    { field: 'make'},
+    { field: 'model'},
+    { field: 'price' }
+  ];
+
+  // DefaultColDef define comandos padrões a todas as colunas
+  public defaultColDef: ColDef = {
+    sortable: true,
+    filter: true,
+  };
+
+  // Variável onde será armazenados os dados que seram exibidos no grid 
+  public rowData$!: Observable<any[]>;
+  //OBS..
+  // Observable é uma funcionalidade da biblioteca rxjs, que é utilizada 
+  // internamente pelo framework e já é instalada quando você cria uma nova 
+  // aplicação Angular. Com Observables, conseguimos lidar com transferência 
+  // de dados assíncrona.
+
+  // Para acessar o Grid's API
+  @ViewChild(AgGridAngular) agGrid!: AgGridAngular;
+
+  constructor(private http: HttpClient) {}
+
+  // Exemplo de carregamento de dados do servidor
+  onGridReady(params: GridReadyEvent) {
+    this.rowData$ = this.http.get<any[]>('https://www.ag-grid.com/example-assets/row-data.json');
+  }
+
+  // Exemplo de carregamento de dados do servidor
+  onCellClicked( e: CellClickedEvent): void {
+    console.log('cellClicked', e);
+  }
+
+  // Exemplo usando Grid's API
+  clearSelection(): void {
+    this.agGrid.api.deselectAll();
+  }
 }
