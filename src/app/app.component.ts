b7fc2a37babf68f164c390ee618d87ae280f4342
Humberto Brites <humberto00b@gmail.com>
2022-12-16 10:47:33 -0300

    atualizações

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, ViewChild } from '@angular/core';
 import { AgGridAngular } from 'ag-grid-angular';
-import { CellClickedEvent, ColDef, GridReadyEvent } from 'ag-grid-community';
+import { CellClickedEvent, ColDef, ColGroupDef, GridReadyEvent, SideBarDef } from 'ag-grid-community';
+import 'ag-grid-enterprise';
 import { Observable } from 'rxjs';
+
+import { Propriedades } from './prop_medicoes';
 
 
 @Component({
@@ -12,21 +15,88 @@ import { Observable } from 'rxjs';
 })
 export class AppComponent {
 
+  public rowGroupPanelShow: 'always' | 'onlyWhenGrouping' | 'never' = 'always';
+  public pivotPanelShow: 'always' | 'onlyWhenPivoting' | 'never' = 'always';
+  public sideBar: SideBarDef | string | string[] | boolean | null = {
+    toolPanels: ['columns'],
+  };
+
+  public gridApi: any;
+  public gridColumnApi: any;
+
+
   // Cada definição de coluna resulta em uma coluna.
-  public columnDefs: ColDef[] = [
-    { field: 'make'},
-    { field: 'model'},
-    { field: 'price' }
+  public columnDefs: (ColDef | ColGroupDef)[] = [ 
+    { 
+      field: 'titulo', 
+      headerName: 'Título da Medição',
+      headerCheckboxSelection: true,
+      checkboxSelection: true,
+      showDisabledCheckboxes: true, 
+      rowDrag: true,
+      minWidth: 400  
+    },
+    { 
+      field: 'desc_situacao_contestacao', 
+      headerName: 'Aferição',
+      minWidth: 200 
+    },
+    { 
+      field: 'tipo_de_contagem', 
+      headerName: 'Tipo de Contagem',
+      minWidth: 200 
+    },
+    { 
+      field: 'nivel_detalhe', 
+      headerName: 'Nível de Detalhe',
+      minWidth: 200 
+    },
+    { 
+      field: 'situacao_contagem', 
+      headerName: 'Situação',
+      minWidth: 200 
+    },
+    { 
+      field: 'total_medicao_ifpug', 
+      headerName: 'PF Líquido',
+      minWidth: 200 
+    },
+    { 
+      field: 'esforco', 
+      headerName: 'Esforço',
+      minWidth: 200 
+    },
+    { 
+      field: 'custo', 
+      headerName: 'Custo', 
+      minWidth: 200 
+    },
+    { 
+      field: 'criado_por', 
+      headerName: 'Criado Por',
+      minWidth: 200 
+    },
+    { 
+      field: 'alterado_por', 
+      headerName: 'Modificado Por',
+      minWidth: 200 
+    }
   ];
 
   // DefaultColDef define comandos padrões a todas as colunas
   public defaultColDef: ColDef = {
-    sortable: true,
-    filter: true,
+    sortable: true, 
+    resizable: true,
+    floatingFilter: true,
+    filter: 'agTextColumnFilter',
+    enableRowGroup: true,
+    enablePivot: true,
+    enableValue: true,
+    flex: 1
   };
 
   // Variável onde será armazenados os dados que seram exibidos no grid 
-  public rowData$!: Observable<any[]>;
+  public rowData$!: Observable<Propriedades[]>;
   //OBS..
   // Observable é uma funcionalidade da biblioteca rxjs, que é utilizada 
   // internamente pelo framework e já é instalada quando você cria uma nova 
@@ -36,11 +106,15 @@ export class AppComponent {
   // Para acessar o Grid's API
   @ViewChild(AgGridAngular) agGrid!: AgGridAngular;
 
+
+
   constructor(private http: HttpClient) {}
 
   // Exemplo de carregamento de dados do servidor
   onGridReady(params: GridReadyEvent) {
-    this.rowData$ = this.http.get<any[]>('https://www.ag-grid.com/example-assets/row-data.json');
+    this.gridApi = params.api;
+    this.gridColumnApi = params.columnApi;
+    this.rowData$ = this.http.get<any[]>('assets/medicoes.json');
   }
 
   // Exemplo de carregamento de dados do servidor
@@ -48,8 +122,5 @@ export class AppComponent {
     console.log('cellClicked', e);
   }
 
-  // Exemplo usando Grid's API
-  clearSelection(): void {
-    this.agGrid.api.deselectAll();
-  }
+
 }
---
+++ src/app/prop_medicoes.ts
@@ -0,0 +1,12 @@
+export interface Propriedades {
+    titulo?:string;
+    desc_situacao_contestacao?:string;
+    tipo_de_contagem?:string; 
+    nivel_detalhe?:string;
+    situacao_contagem?:number;
+    total_medicao_ifpug?:number;
+    esforco?:number; 
+    custo?:number; 
+    criado_por?:string; 
+    alterado_por?:string;
+}
